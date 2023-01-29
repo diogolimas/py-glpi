2f948089de3d367a6d335ea10593c525380b2356
Diogo Lima <diogo.lima@zapisp.com.br>
2023-01-29 19:06:18 -0300

    refactor: verificação no GLPI + setup dos ambientes.

--- py-glpi.py
+++ py-glpi.py
@@ -1,28 +1,50 @@
 #!/usr/bin/env python
 #coding:utf-8
-
+import glpi_api
 import json
-from glpi import GLPI
-from glpi import GlpiProfile
-from glpi import GlpiTicket, Ticket
+import discord
+from datetime import datetime, timedelta
 from pprint import pprint
 import logging
-import ConfigParser
+import configparser
 import telepot
+import os
+from dotenv import load_dotenv
 
-# Читаем конфиг
-conf = ConfigParser.RawConfigParser()
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+CHAT_ID_TELEGRAM = os.getenv('IDCHAT_TELEGRAM')
+CHANEL_DISCORD = os.getenv('CHANEL_DISCORD')
+conf = configparser.ConfigParser()
+conf.sections()
 conf.read("config.ini")
+listMensagens = []
 
-# секция glpi, посвященная подключению к API
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+        for chamado in listMensagens:
+            channel = client.get_channel(CHANEL_DISCORD)
+            await channel.send(chamado)
+
+    async def on_message(self, message):
+        if message.author == client.user:
+            return
+        channel = client.get_channel(CHANEL_DISCORD)
+        await channel.send('👀')
+        print(f'Message from {message.author}: {message.content}')
+
+
 try:
     glpi_url = conf.get("glpi", "url")
-    glpi_user = conf.get("glpi", "user")
-    glpi_password = conf.get("glpi", "password")
+    glpi_apptoken = conf.get("glpi","apptoken")
     glpi_token = conf.get("glpi", "token")
 except ConfigParser.NoSectionError:
     logging.error(u"Can't read glpi section in config.ini")
     raise SystemExit(1)
+
 
 # секция telegram, посвященная подключению к telegram-api
 try:
@@ -30,19 +52,53 @@ try:
 except ConfigParser.NoSectionError:
     logging.error(u"Can't read telegram section in config.ini")
     raise SystemExit(1)
-glpi = GLPI(glpi_url, glpi_token, (glpi_user, glpi_password))
-bot = telepot.Bot(telegram_token)
 
+
+criteria = [ {
+      "link":"AND",
+      "field":"12",
+      "searchtype":"equals",
+      "value":"notold"
+    },
+    {
+      "link":"AND",
+      "field":"5",
+      "searchtype":"contains",
+      "value":"diogo da silva lima"
+    }]
+
+force_display=[1,2,5,19]  
+
+chamadosAtualizacao = []
+
+try:
+    with glpi_api.connect(glpi_url, glpi_apptoken, glpi_token, False) as glpi:
+        chamados = glpi.search('Ticket', criteria=criteria,forcedisplay=force_display)
+        pprint(chamados)
+        print(type(chamados))
+        d = datetime.today() - timedelta(hours=0, minutes=30)
+        print(d)
+        atualizacoes = filter(lambda c: datetime.strptime(c['19'], '%Y-%m-%d %H:%M:%S') > d, chamados)
+        chamadosAtualizacao = list(atualizacoes)
+
+except glpi_api.GLPIError as err:
+    print('error '+str(err))
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+#glpi = GLPI(glpi_url, glpi_apptoken, glpi_token)
+bot = telepot.Bot(telegram_token)
 bot.getMe()
 response = bot.getUpdates()
-pprint(response)
-#bot.sendMessage(180318137, "!Privet it is Python Bot :)")
-#logging.warning(u'Test warning MESSAGE')
 
-"""
-print "Getting all Tickets: "
-print json.dumps(glpi.get_all('ticket'),
-                  indent=4,
-                  separators=(',', ': '),
-                  sort_keys=True)
-"""
+for chamado in chamadosAtualizacao:
+    messageTelegram = ".\n\nATENÇÃO🚨\n\nvocê tem um chamado que foi atualizado nos últimos 30 minutos\nchamado: {numChamado}\nacesse em: https://glpi.tce.rn.gov.br/front/ticket.form.php?id={numChamado}\n{tituloChamado}".format(numChamado=chamado['2'],tituloChamado=chamado['1'])
+    listMensagens.append(messageTelegram)
+    print(messageTelegram)
+    bot.sendMessage(CHAT_ID_TELEGRAM, messageTelegram)
+
+
+client = MyClient(intents=intents)
+client.run(TOKEN)
